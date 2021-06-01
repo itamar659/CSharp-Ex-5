9d28cc5dd3c61fdafa5a3cae636b56d0a8e974a8
Yaniv Avrahami <avrahamiyaniv@gmail.com>
2021-06-01 21:56:04 +0300

    asdasd

--- B21 Ex05 Yaniv 302374921 Itamar 206024796/Ex05.XMixDrixReverse.UI/FormGameSettings.cs
+++ B21 Ex05 Yaniv 302374921 Itamar 206024796/Ex05.XMixDrixReverse.UI/FormGameSettings.cs
@@ -37,29 +37,40 @@ namespace Ex05.XMixDrixReverse.UI
         private void initializeComponents()
         {
             //Row 1
-            //m_LabelPlayers = new Label();
-            //m_LabelPlayers.Text
+            m_LabelPlayers = new Label();
+            m_LabelPlayers.Text = "Players";
+            m_LabelPlayers.Left = 16;
+            m_LabelPlayers.Top = 16;
+            this.Controls.Add(m_LabelPlayers);
 
             //Row 2
-            Label m_LabelPlayer1;
-            TextBox m_TextBoxPlayer1;
+            m_LabelPlayer1 = new Label();
+            m_LabelPlayer1.Text = "Player 1";
+            m_LabelPlayer1.Left = 16;
+            m_LabelPlayer1.Top = 16;
+            this.Controls.Add(m_LabelPlayer1);
 
-            //Row 3
-            CheckBox m_CheckBoxIsMultiplayer;
-            Label m_LabelPlayer2;
-            TextBox m_TextBoxPlayer2;
+            m_TextBoxPlayer1 = new TextBox();
+            m_TextBoxPlayer1.Left = 16;
+            m_TextBoxPlayer1.Top = 16;
+            this.Controls.Add(m_TextBoxPlayer1);
 
-            //Row 4
-            Label m_LabelBoardSize;
-
-            //Row 5
-            Label m_LabelRows;
-            NumericUpDown m_NumericUpDownRows;
-            Label m_LabelCols;
-            NumericUpDown m_NumericUpDownCols;
-
-            //Row 6
-            Button m_ButtonStart;
+            ////Row 3
+            //m_CheckBoxIsMultiplayer = ;
+            //m_LabelPlayer2;
+            //m_TextBoxPlayer2;
+            //
+            ////Row 4
+            //m_LabelBoardSize;
+            //
+            ////Row 5
+            //m_LabelRows;
+            //m_NumericUpDownRows;
+            //m_LabelCols;
+            //m_NumericUpDownCols;
+            //
+            ////Row 6
+            //m_ButtonStart;
         }
     }
 }
--- B21 Ex05 Yaniv 302374921 Itamar 206024796/Ex05.XMixDrixReverse.UI/FormXMixDrixReverse.cs
+++ B21 Ex05 Yaniv 302374921 Itamar 206024796/Ex05.XMixDrixReverse.UI/FormXMixDrixReverse.cs
@@ -1,10 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Windows.Forms;
 
 namespace Ex05.XMixDrixReverse.UI
 {
-    class FormXMixDrixReverse
+    class FormXMixDrixReverse : Form
     {
     }
 }
